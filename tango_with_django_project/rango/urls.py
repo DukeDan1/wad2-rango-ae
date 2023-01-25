2c9ddd6ec53f34d7d5076e200d9e7aaccdd744f8
Daniel Shields <88106441+DukeDan1@users.noreply.github.com>
2023-01-25 02:30:46 +0000

    Unit testing for chapter 3 completed. Tests pass.

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -18,10 +18,11 @@ from django.urls import path
 from django.urls import include
 from rango import views
 
+app_name = 'rango'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    
+    path('about/', views.about, name='about'),    
 ]
 
 
